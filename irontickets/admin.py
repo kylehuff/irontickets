e0f5b9f925684756980b658e7ac8286e8e376bf7
Aaron C. de Bruyn <aaron@heyaaron.com>
2010-01-11 01:11:07 -0800

    Added inline for attachments when viewing tickets in admin

--- irontickets/admin.py
+++ irontickets/admin.py
@@ -1,9 +1,13 @@
 from models import *
 from django.contrib import admin
+from attachments.admin import AttachmentInlines
 
 class TicketPriorityAdmin(admin.ModelAdmin):
     list_display = ('priority', 'name', 'showinportal')
     ordering = ('priority', 'name')
+
+class TicketAdmin(admin.ModelAdmin):
+    inlines = [AttachmentInlines]
 
 admin.site.register(TicketPriority, TicketPriorityAdmin)
 
@@ -12,7 +16,7 @@ admin.site.register(CompanyStatus)
 admin.site.register(Company)
 admin.site.register(TicketStatus)
 admin.site.register(TicketSource)
-admin.site.register(Ticket)
+admin.site.register(Ticket, TicketAdmin)
 admin.site.register(ContactTitle)
 admin.site.register(AddressType)
 admin.site.register(Address)
