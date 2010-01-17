87ec1051c800ca11e5c0aa05724abaf710046c52
Aaron C. de Bruyn <aaron@heyaaron.com>
2010-01-16 16:36:18 -0800

    Switched to realtime indexing

--- irontickets/search_indexes.py
+++ irontickets/search_indexes.py
@@ -3,7 +3,7 @@ from haystack import indexes
 from haystack import site
 from irontickets.models import Ticket
 
-class TicketIndex(indexes.SearchIndex):
+class TicketIndex(indexes.RealTimeSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
 
 site.register(Ticket, TicketIndex)
